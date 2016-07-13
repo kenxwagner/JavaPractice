d2cef1e41e38b15a4e52b4a5ef29f806e77b604d
Ken Wagner <ken_xwagner@hotmail.com>
2016-07-13 02:04:27 -0700

    more text added

--- JavaJuly/src/AboutTheWorld.java
+++ JavaJuly/src/AboutTheWorld.java
@@ -1,7 +1,9 @@
 public class AboutTheWorld
 {
+	static String randomString = "This is the class variable, which is accessible by any method or function";
 	public static void main(String[] args)
 	{
 		System.out.println("Let me tell you about the world");
+		System.out.println(randomString);
 	}
 }
