3a9dd571a4fdeb34f5ba8c9a1aabc45855eef4d2
Ken Wagner <ken_xwagner@hotmail.com>
2016-07-13 02:28:17 -0700

    more class
    adding classes

---
+++ JavaJuly/src/Import.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Import {
+
+	static Scanner userInput = new Scanner(System.in);
+	
+	public static void main(String[]args)
+	{
+		System.out.print("Give the number of the episode you like of Star Wars: ");
+		
+		if (userInput.hasNextInt())
+		{
+			int numberEntered = userInput.nextInt();
+			
+			System.out.println("You like Episode " + numberEntered);
+		}
+		
+	}
+}
